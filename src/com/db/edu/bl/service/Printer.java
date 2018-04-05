591cc5ddb912a8b75480355a55e4d158d42ecdeb
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-04-05 10:45:31 +0300

    Add packages demo.

---
+++ src/com/db/edu/bl/service/Printer.java
@@ -0,0 +1,7 @@
+package com.db.edu.bl.service;
+
+public class Printer {
+	public void print(String message) {
+		System.out.println(message);
+	}
+}
---
+++ src/com/db/edu/infr/App.java
@@ -0,0 +1,10 @@
+package com.db.edu.infr;
+
+import com.db.edu.bl.service.Printer;
+
+public class App {
+	public static void main(String... arg) {
+		Printer printer = new Printer();
+		printer.print("HW!!!");
+	}
+}

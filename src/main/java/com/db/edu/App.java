a2f85828aa3cbf8473ebdbd9b06f1a62544f3aa2
sakilan <sakilan@web2mailco.com>
2018-04-09 12:08:03 +0300

    Create mvn project

---
+++ src/main/java/com/db/edu/App.java
@@ -0,0 +1,13 @@
+package com.db.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

0592e4d9f4cf361406e934cff509fc7b63cc5a72
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-04-26 11:18:11 +0300

    GC object availability.

--- src/test/java/com/db/edu/LoggerIT.java
+++ src/test/java/com/db/edu/LoggerIT.java
@@ -5,8 +5,12 @@ import com.db.edu.logger.api.LoggerController;
 public class LoggerIT {
     public static void main(String[] args) {
         LoggerController logger = new LoggerController();
-        logger.log("test message");
 
+        LoggerController var = new LoggerController();
+        var = null;
+        System.gc(); //Not garanteed GC!!!
+
+        logger.log("test message");
 //        assertThatMessageInConsoleEqualsTo(">> test message");
     }
 }

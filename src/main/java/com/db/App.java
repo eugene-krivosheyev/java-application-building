bc28ace6d47040b6fa115a8aa0c26184b2e8b365
dropnz <dropnz.fw@gmail.com>
2021-02-04 14:42:22 +0300

    added hello world

---
+++ src/main/java/com/db/App.java
@@ -0,0 +1,10 @@
+package com.db.hw;
+
+import com.db.hw.service.Printer;
+
+public class App {
+    public static void main( String[] args ) {
+	Printer printer = new Printer();
+	printer.print("HW!");
+    }
+}
---
+++ src/main/java/com/db/service/Printer.java
@@ -0,0 +1,7 @@
+package com.db.hw.service;
+
+public class Printer {
+    public void print(String inputString) {
+	System.out.println(inputString);
+    }
+} 

1cde9c76e817e873821d58504ce9662261893567
Alexandr Sidorov <Sidorov.Alexandr@gmail.com>
2021-02-07 23:18:04 +0300

    Add executable pom and correct source files

---
+++ src/main/java/com/db/edu/hw/App.java
@@ -0,0 +1,11 @@
+package com.db.edu.hw;
+
+import com.db.edu.hw.service.Printer;
+
+public class App {
+	public static void main (String[] arg){
+	//	System.out.println ("HW");
+		Printer printer = new Printer ();
+		printer.print("HW");
+	}
+}

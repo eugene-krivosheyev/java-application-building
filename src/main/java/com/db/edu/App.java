b992d59733d62eff154f51ba9fd84ac59858c4b0
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2021-02-09 09:53:01 +0300

    adds printer service, removed test

---
+++ src/main/java/com/db/edu/App.java
@@ -0,0 +1,11 @@
+package com.db.edu;
+
+import com.db.edu.service.Printer;
+
+public class App {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.print("Hello World!");
+        System.out.println("int");
+    }
+}

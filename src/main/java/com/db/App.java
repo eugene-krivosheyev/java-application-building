70361a2df6d526fe2e4336fbd7bc51eb74ab6231
Alex V. Solo <lexxan@bk.ru>
2021-02-09 09:53:34 +0300

    PR test

--- src/main/java/com/db/App.java
+++ src/main/java/com/db/App.java
@@ -4,10 +4,11 @@ package com.db;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.db.service.Printer;
+
+public class App {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.print("Hello World!");
     }
 }
---
+++ src/main/java/com/db/service/Printer.java
@@ -0,0 +1,7 @@
+package com.db.service;
+
+public class Printer {
+	public void print(String msg) {
+		System.out.println(msg);
+	}
+}

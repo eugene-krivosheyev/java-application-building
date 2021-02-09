fc73d6339dea54ad075706ab227899a5852dc40c
AEfremov12 <alexandrovefim@mail.ru>
2021-02-09 14:43:22 +0300

    Changed message

--- src/main/java/com/db/App.java
+++ src/main/java/com/db/App.java
@@ -5,7 +5,7 @@ package com.db;
  */
 public class App {
     public static void main( String[] args ) {
-       System.out.println( "Hello World!" );
+       System.out.println( "Hello Worlds!" );
     }
 }
 

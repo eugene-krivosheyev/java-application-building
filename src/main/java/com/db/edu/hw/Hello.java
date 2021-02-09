a0d6487e8f21ea3e8141d67adb6cb5728cedf579
danilishe <suetin.danil@gmail.com>
2021-02-09 10:41:04 +0400

    add old project's files

---
+++ src/main/java/com/db/edu/hw/Hello.java
@@ -0,0 +1,9 @@
+package com.db.edu.hw;
+
+import com.db.edu.hw.service.Printer;
+
+public class Hello {
+    public static void main(String... args) {
+        new Printer().print("Hello World");
+    }
+}

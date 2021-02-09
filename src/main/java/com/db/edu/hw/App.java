37c7aa257155acc056c81ec64d18590622c44658
hoakim92 <361629Qaz>
2021-02-09 09:35:46 +0300

    initial commit

---
+++ src/main/java/com/db/edu/hw/App.java
@@ -0,0 +1,10 @@
+package com.db.edu.hw;
+
+import com.db.edu.hw.service.Printer;
+
+public class App {
+	public static void main(String[] args) {
+		Printer printer = new Printer();
+		printer.print("HW!!!");
+	} 
+}
---
+++ src/main/java/com/db/edu/hw/service/Printer.java
@@ -0,0 +1,7 @@
+package com.db.edu.hw.service;
+
+public class Printer {
+	public static void print(String message) {
+		System.out.println(message);
+	}
+}

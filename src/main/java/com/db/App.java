f20e6156665e3d7c51e017a53d16a387097a89d8
AlexNekh12 <mpostbox@inbox.ru>
2021-02-09 15:16:08 +0300

    Formatting

--- src/main/java/com/db/App.java
+++ src/main/java/com/db/App.java
@@ -4,8 +4,9 @@ package com.db;
  * Hello world!
  */
 public class App {
-    public static void main( String[] args ) {
-       System.out.println( "Hello World!" );
+    public static void main( String[] args )
+    {
+       System.out.println( "Hello from AN(group 2)!" );
     }
 }
 

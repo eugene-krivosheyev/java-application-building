b8136d5578bad04f955451e9b162bb11f9899b44
rmatafonov <matafonov.rv@gmail.com>
2021-02-09 09:37:52 +0300

    Corrected pom.xml; added Printer.java and new App.java

---
+++ src/main/java/com/db/edu/rmatafonov/App.java
@@ -0,0 +1,9 @@
+package com.db.edu.rmatafonov;
+
+import com.db.edu.rmatafonov.service.Printer;
+
+public class App {
+	public static void main(String[] args) {
+		new Printer().print("Hello!");
+	}
+}
---
+++ src/main/java/com/db/edu/rmatafonov/service/Printer.java
@@ -0,0 +1,7 @@
+package com.db.edu.rmatafonov.service;
+
+public class Printer {
+	public void print(String message) {
+		System.out.println(message);
+	}
+}

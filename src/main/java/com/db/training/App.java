5e4385c7b0ae6dce89763b35cc73d45f66a3caaf
Michael Shabynin <vectro73@gmail.com>
2021-02-09 14:45:36 +0300

    -Add maven project -Add Main program -Add Printer module

---
+++ src/main/java/com/db/training/App.java
@@ -0,0 +1,10 @@
+package com.db.training;
+
+import com.db.training.services.Printer;
+
+public class App {
+	public static void main(String[] args) {
+		Printer printer = new Printer();
+		printer.print("Hello world!");
+	}
+}
---
+++ src/main/java/com/db/training/services/Printer.java
@@ -0,0 +1,6 @@
+package com.db.training.services;
+public class Printer {
+	public void print(String data) {
+		System.out.println(data);
+	}	
+}

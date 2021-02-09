f14c469636fcb0549d5b3c332a8bbdb95c6d5087
Alexey Tarasovskiy <alexey.tarasovskiy@gmail.com>
2021-02-09 15:10:24 +0300

    Adding a simple comment //Trying a simple PR2

--- src/main/java/com/db/App.java
+++ src/main/java/com/db/App.java
@@ -6,7 +6,7 @@ package com.db;
 //Trying a simple PR
 public class App {
     public static void main( String[] args ) {
-       System.out.println( "Hello World!!!" );
+       System.out.println( "Hello World!!!!!" );
     }
 }
 
